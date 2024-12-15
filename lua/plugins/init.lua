61346e95a3428ad18cee036ab4fb8663bb427440
MojaHead <aziz.yubuza@mail.ru>
2024-12-15 19:29:02 +0300

    set up dash board

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,7 +6,18 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-
+{
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    local dash_board_settings = require('dashboard').setup {
+        theme = 'hyper',
+      }
+    
+    
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+},
   {
     "williamboman/mason.nvim",
 
