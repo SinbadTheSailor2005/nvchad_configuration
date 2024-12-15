27b1ba6a1d63192743ac8ab6feba168548928d3f
MojaHead <aziz.yubuza@mail.ru>
2024-12-15 19:48:38 +0300

    set up c++ debugger

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,6 +31,69 @@ return {
       },
     },
   },
+
+{
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    config = function ()
+    require("configs.debuggers.debugger_cpp")
+    end,
+      },
+  --  {
+  --   "mfussenegger/nvim-dap",
+  --   config = function()
+  --     require("dap").adapters.codelldb = {
+  --       type = "server",
+  --       port = "${port}",
+  --       executable = {
+  --         command = "codelldb",
+  --         args = { "--port", "${port}" },
+  --       },
+  --     }
+  --     require("dap").configurations.cpp = {
+  --       {
+  --         name = "Launch codelldb",
+  --         type = "codelldb", -- Используем тип, который настроен выше
+  --         request = "launch",
+  --         program = function()
+  --           return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/main", "file")
+  --         end,
+  --         cwd = "${workspaceFolder}", -- Рабочая директория
+  --         stopOnEntry = false, -- Не останавливать выполнение сразу
+  --         args = {}, -- Аргументы для программы
+  --         runInTerminal = true, -- Не запускать в отдельном терминале
+  --         console = "integratedTerminal",
+  --
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
     config = function()
