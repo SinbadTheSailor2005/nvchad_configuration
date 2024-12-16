41561ca514e189b7ab6b70d51d9d2d561d8b6bb5
MojaHead <aziz.yubuza@mail.ru>
2024-12-16 10:05:28 +0300

    bug fix

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -65,35 +65,6 @@ return {
     require("configs.debuggers.debugger_cpp")
     end,
       },
-  --  {
-  --   "mfussenegger/nvim-dap",
-  --   config = function()
-  --     require("dap").adapters.codelldb = {
-  --       type = "server",
-  --       port = "${port}",
-  --       executable = {
-  --         command = "codelldb",
-  --         args = { "--port", "${port}" },
-  --       },
-  --     }
-  --     require("dap").configurations.cpp = {
-  --       {
-  --         name = "Launch codelldb",
-  --         type = "codelldb", -- Используем тип, который настроен выше
-  --         request = "launch",
-  --         program = function()
-  --           return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/main", "file")
-  --         end,
-  --         cwd = "${workspaceFolder}", -- Рабочая директория
-  --         stopOnEntry = false, -- Не останавливать выполнение сразу
-  --         args = {}, -- Аргументы для программы
-  --         runInTerminal = true, -- Не запускать в отдельном терминале
-  --         console = "integratedTerminal",
-  --
-  --       },
-  --     }
-  --   end,
-  -- },
   {
     "neovim/nvim-lspconfig",
     config = function()
