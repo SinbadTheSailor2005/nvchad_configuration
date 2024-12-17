86ee55b015753208bc4f7e86c1bcbf9802cda86d
MojaHead <aziz.yubuza@mail.ru>
2024-12-17 16:04:09 +0300

    bug fix

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,6 +41,7 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
+    lazy = false,
   },
   {
     "rcarriga/nvim-dap-ui",
