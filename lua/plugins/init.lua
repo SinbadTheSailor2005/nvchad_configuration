4edc1922268f98737c33dc20ff78ea94f1cff3fa
Aziz_Arch_Desktop <aziz.yubuza@mail.ru>
2025-01-08 22:19:49 +0500

    add extra md plugin (unneccesary??)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,6 +52,16 @@ return {
     end,
   },
 {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    ft = {"markdown"},
+},
+{
   "ibhagwan/fzf-lua",
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- СКАЧАЙ САМ fzf через apt...
