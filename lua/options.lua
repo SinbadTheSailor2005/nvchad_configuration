d0ec54f11f3019c113b19ce6d8bfe94045d760a3
Aziz_Arch_Desktop <aziz.yubuza@mail.ru>
2025-01-09 11:48:18 +0500

    change md plugin(also support html)

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,12 @@
 require "nvchad.options"
-
+require('livepreview.config').set({
+	port = 5500, 
+	autokill = false, 
+	browser = 'default', 
+	dynamic_root = false, 
+	sync_scroll = true,
+	picker = "telescope", 
+    })
 -- add yours here!
 
 -- local o = vim.o
